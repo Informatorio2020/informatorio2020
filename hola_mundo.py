293b88fb2199b4af31be673b02818d98ba9e2d3a
Mariana Núñez <38579384+MarianaNun@users.noreply.github.com>
2020-08-03 04:16:26 -0300

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -7,8 +7,6 @@ print("Cambios hechos en la rama")
 print("Cambios del local")
 print("nuevos cambios desde el remoto")
 print("Mas cambios desde el remoto")
-<<<<<<< HEAD
 print("Cambios hechos para probar el merge")
-=======
 print("Mas cambios desde el remoto para probar el merge")
->>>>>>> ed3ace20f7c8102cc24de83387d2adba0eab888a
+print("nuevos cambios desde el remoto para probar el rebase")
