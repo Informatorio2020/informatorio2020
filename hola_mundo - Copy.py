7d198dd1ed12834fd90377baa9810779682a41a0
mariana.nunez <mariana.nunez10@gmail.com>
2020-08-03 03:15:33 -0300

    Eliminamos la copia y agregamos una nueva linea

--- hola_mundo - Copy.py
+++
@@ -1,3 +0,0 @@
-print("Hola mundo")
-print("clase de versionado")
-print("mas cambios")
--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,4 @@
 print("Hola mundo")
 print("clase de versionado")
 print("mas cambios")
+print("Agregados con rebase")
