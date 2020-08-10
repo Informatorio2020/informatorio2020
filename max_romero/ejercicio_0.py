2dd10d6c92a9d11e42c69e663ca695ddeddf3e8d
Rivochill <romero.max44@gmail.com>
2020-08-10 19:03:34 -0300

    Creacion de la clase Contacto y definicion de la misma

--- max_romero/ejercicio_0.py
+++ max_romero/ejercicio_0.py
@@ -1,1 +1,17 @@
-print('Hello Informatorio 2020')
+"""CASO 4: Realizar una clase que administre una agenda.
+Se debe almacenar para cada contacto el nombre, el teléfono y el email. 
+Además deberá mostrar un menú con las siguientes opciones.
+
+- Añadir contacto
+- Lista de contactos
+- Buscar contacto
+- Editar contacto
+- Cerrar agenda
+"""
+
+class Contacto: 
+    def __init__(self, nombre, telefono, email):
+        self.nombre=nombre
+        self.telefono=telefono
+        self.email=email
+
