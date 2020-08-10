eeca83a91c58e261ae2b85e242bcdfc4c94be8c8
nelgus <nego16887@gmail.com>
2020-08-10 19:07:32 -0300

    Agregamos la clase Contacto

---
+++ Nelgusgom/agenda.py
@@ -0,0 +1,16 @@
+#Realizar una clase que administre una agenda. Se debe almacenar para cada contacto el nombre
+#el teléfono y el email. Además deberá mostrar un menú con las siguientes opciones.
+# Añadir contacto
+# Lista de contactos
+# Buscar contacto
+# Editar contacto
+# Cerrar agenda
+
+class Contacto():
+
+	def __init__(self, nombre, telefono, email):
+		self.nombre = nombre
+		self.telefono = telefono
+		self.email = email
+
+	
