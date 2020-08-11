71e6f96811cf1fbe812f4a2f7765b43c6cdf9304
Jimmy <rodrigonieto.1993@gmail.com>
2020-08-11 19:42:35 -0300

    . . .

--- rodrigo_nieto/agenda.py
+++ rodrigo_nieto/agenda.py
@@ -1,8 +1,49 @@
+"""
+Esto tengo hasta ahora 
+no se si estará bien o no
+pero es lo que me va saliendo
+"""
+
 class Contacto:
 	def __init__(self,nombre,telefono,email):	
 		self.nombre = nombre
-		self.dni = telefono
+		self.telefono = telefono
 		self.email = email
-		
 
+#Método agregar: agrega contactos
+	def agregar(self):
+		agenda[self.nombre] = [self.telefono, self.email]
+
+#Método lista: muestra lista de contactos	
+	def lista(self):
+		lista_Contactos.append(self.nombre)
+#Buscar contacto: busca contacto por nombre
+	def getnombre(self):
+		return self.nombre
+#Editar contacto: Edita contacto
+	def setnombre(self,a):
+		self.nombre = a
+
+	def setemail(self,b):
+		self.email = b
+		
+	def settelefono(self,c):
+		self.telefono = c
+
+	
+
+
+
+lista_Contactos = []
+agenda = {}
+
+nombre = input('Nombre: ')
+telefono = input('Telefono: ')
+email = input('Email: ')
+
+registro = Contacto(nombre,telefono, email)
+registro.agregar()
+registro.lista()
+
+print(registro)
 
