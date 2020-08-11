ccb519a8304c5f3b49ce1647f6e03db2186a51d4
paula <paulaflorensia@gmail.com>
2020-08-11 19:43:59 -0300

    Actualizacion caso_4

--- Paula Herrera/caso_4.py
+++ Paula Herrera/caso_4.py
@@ -4,3 +4,39 @@ class Contacto():
 		self.nombre = nombre
 		self.telefono = telefono
 		self.email = email
+
+#Solucion 2
+#Metodo
+'''
+	def aniadir(self):
+		agenda[self.nombre] = [self.telefono, self.email]
+'''
+
+class Agenda():
+	def __init__(self):
+		self.lista_de_contactos = []
+
+	def aniadir(self, dato):
+		self.lista_de_contactos.append(dato)
+
+agenda = Agenda()
+
+nombre_usuario = input('Nombre: ')
+telefono_usuario = input('Telefono: ')
+email_usuario = input('Email: ')
+
+registro = Contacto(nombre_usuario, telefono_usuario, email_usuario)
+
+agenda.aniadir(registro)
+
+print(agenda.lista_de_contactos)
+#registro.aniadir()
+
+#Solucion 1
+#funcion
+'''
+def aniadir(datos):
+	agenda[datos.usuario] = [datos.telefono, datos.email]
+
+aniadir(registro)
+'''
