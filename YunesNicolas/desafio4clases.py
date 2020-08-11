667fe24748554d1f1df531ecd47fdba63253fff4
nico <nicolaseduardoyunes@gmail.com>
2020-08-11 19:04:22 -0300

    commit 1

---
+++ YunesNicolas/desafio4clases.py
@@ -0,0 +1,9 @@
+class Contacto():
+    lista = []
+    def __init__(self, nombre, telefono, email):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.email = email
+
+    def añadir_contacto(lista):
+        
