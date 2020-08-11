fc7f2f40fee5ef73a1135e05516675a4460d3a30
Fernando <pipao.pipo@gmail.com>
2020-08-11 19:42:15 -0300

    varias opciones

--- Fernando_enrique/agenda.py
+++ Fernando_enrique/agenda.py
@@ -1,14 +1,103 @@
 
 
 
+
+
 class Contacto():
+	agenda = []
 	def __init__(self,nombre, telefono,mail):
 		self.nombre=nombre
 		self.telefono=telefono
 		self.mail=mail
+		agenda.append(nombre)
 
-		
+		def __str_ (self):
+			return nombre
+
+
+salir = True
+
+while True:
+	nom = input("Ingrese nombre")
+	if nom in agenda:
+		print("contacto existente")
+		continue
+	tel= input("ingrese tel")
+	mail=input ("ingrese mail")
+	contac=contacto(nom,tel,mail)
+
+	op=input("para salir  escriba si")
+	if po == si:
+		salir = False
+
+print (pedro)
 
 
 
-		
+
+
+
+# class Contacto():
+# 	def __init__(self,nombre, telefono,mail):
+# 		self.nombre=nombre
+# 		self.telefono=telefono
+# 		self.mail=mail
+
+
+
+# agenda= []
+
+
+
+	
+# agenda = {}
+# nombre = input("nombre: ")
+# telefono= input("telefono: ")
+# email= input("Email: ")
+# registro = contacto(nombre,telefono,email)
+
+# def aniadir(conacto):
+# 	agenda[contacto.nombre]= (contacro.telefono,contacto.email)
+
+
+
+# def agregar:
+# 	nombre = input("nombre: ")
+# 	telefono= input("telefono: ")
+# 	email= input("Email: ")
+# 	contactoaux = contacto(nombre,telefono,email)
+# 	agenda.append(conttactoaux)
+
+
+
+
+# class Contacto():
+	
+# 	def __init__(self,nombre, telefono,mail):
+# 		self.nombre=nombre
+# 		self.telefono=telefono
+# 		self.mail=mail
+# 		agenda[nombre]= [telefono, mail]
+# 	def __str__ (self):
+# 		return nombre
+
+
+# salir = True
+
+# agenda = []
+# nom = input("Ingrese nombre")
+# tel= input("ingrese tel")
+# mail=input ("ingrese mail")
+# contact = Contacto(nom, tel, mail)
+# nom = input("Ingrese nombre")
+# tel= input("ingrese tel")
+# mail=input ("ingrese mail")
+# contact = Contacto(nom, tel, mail)
+
+# for i in agenda:
+# 	print(i)
+# 	print(agenda.keys())
+
+
+
+
