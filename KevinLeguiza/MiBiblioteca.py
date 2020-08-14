42310781a2a6b1b1102bd8fe00855393d2ba91ab
Kevin Leguiza <NicolasKevinLeguiza@gmail.com>
2020-08-14 17:05:01 -0300

    Mejora de 'MiBiblioteca.pu' - Implementacion de constructor

--- KevinLeguiza/MiBiblioteca.py
+++ KevinLeguiza/MiBiblioteca.py
@@ -1,17 +1,28 @@
 class Escritor():
 
-	def espaciar(self, espaciado):
-		for i in range(0, espaciado):
+	def __init__(self, caracter="*", espaciado=1):
+		self.caracter = caracter
+		self.espaciado = espaciado
+		self.numero_opcion = 1
+
+	def set_espaciado(self, espaciado):
+		self.espaciado = espaciado
+
+	def set_caracter(self, caracter):
+		self.caracter = caracter
+
+	def espaciar(self):
+		for i in range(0, self.espaciado):
 			print(end=" ")
 
-	def crear_espaciado(self, tamano, espaciado):
-		print("*", end="")
+	def crear_espaciado(self, tamano):
+		print(self.caracter, end="")
 		for i in range(0,tamano):
 			print(" ", end="")
-		print("*", end="\n")
-		self.espaciar(espaciado)
+		print(self.caracter, end="\n")
+		self.espaciar()
 
-	def crear_barra(self, tamano, espaciado, titulo=""):
+	def crear_barra(self, tamano, titulo=""):
 		
 		if titulo == "":
 			tamano += 2
@@ -21,35 +32,39 @@ class Escritor():
 			if i == tamano/2 and titulo != "":
 				print("-{}-".format(titulo), end="")
 
-			print("*", end="")
+			print(self.caracter, end="")
 		print(end="\n")
-		self.espaciar(espaciado)
+		self.espaciar()
 
-	def crear_opcion(self, tamano, espaciado, opcion):
-
+	def crear_opcion(self, tamano, opcion, enumeracion):
 		tamano_opcion = int(tamano/2-len(opcion)/2)
-		print("*", end=" ")
+		if enumeracion:
+			aux = "{}).".format(self.numero_opcion)
+			tamano_opcion -= len(aux)
+			opcion = aux + opcion
+			self.numero_opcion += 1
+		print(self.caracter, end=" ")
 		for j in range(0, tamano-len(opcion)-1):
 			if j == tamano_opcion:
 				print(opcion, end="")
 			print(end=" ")
-		print("*", end="\n")
-		self.espaciar(espaciado)
+		print(self.caracter, end="\n")
+		self.espaciar()
 
-	def crear_menu(self, titulo, opciones, mensaje_input="::", tamano=50, espaciado=1):
+	def crear_menu(self, titulo, opciones, enumeracion, tamano=50):
 		print()
 		#BARRA SUPERIOR
-		self.espaciar(espaciado)
-		self.crear_barra(tamano, espaciado, titulo)
+		self.espaciar()
+		self.crear_barra(tamano, titulo)
 		tamano += len(titulo)
 		
 		#CREACION DEL CUERPO
-		for i in range(0,len(opciones)):
-			self.crear_espaciado(tamano, espaciado)
-			self.crear_opcion(tamano, espaciado, opciones[i])
+		for i in range(0, len(opciones)):
+			self.crear_espaciado(tamano)
+			if i >= enumeracion[0] and i <= enumeracion[1]:
 
-		self.crear_espaciado(tamano, espaciado)
-		self.crear_barra(tamano, espaciado)
-		print(end="\n\n")
-		self.espaciar(espaciado)
-		print(mensaje_input, end=" ")
+				self.crear_opcion(tamano, opciones[i], True)
+
+		self.crear_espaciado(tamano)
+		self.crear_barra(tamano)
+		self.numero_opcion = 1
